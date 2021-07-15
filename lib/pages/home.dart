b11e565a48c3ccde8ae02a3fa553cdc353189b86
Sharon Rithika <sharonrithika15@gmail.com>
2021-07-15 21:16:06 +0530

    adding files

---
+++ lib/pages/home.dart
@@ -0,0 +1,95 @@
+
+
+//import 'dart:js';
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+Map data={};
+
+  @override
+  Widget build(BuildContext context) {
+
+    data = data.isNotEmpty? data: ModalRoute.of(context)!.settings.arguments as Map;
+    print(data);
+
+    String bgImage= data['isDaytime'] ? 'day.png':'night.png';
+    Color? bgColor= data['isDaytime'] ? Colors.blue : Colors.indigo[700];
+
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: SafeArea(child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/pics/$bgImage'),
+            fit: BoxFit.cover,
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+          child: Column(
+            children: [
+              TextButton.icon(
+                onPressed: () async {
+                  dynamic result=await Navigator.pushNamed(context, '/location');
+                  setState(() {
+                    data ={
+                      'time': result['time'],
+                      'location': result['location'],
+                      'isDaytime': result['isDaytime'],
+                      'flag': result['flag'],
+
+                    };
+                  });
+                }, 
+                icon: Icon(
+                  Icons.edit_location,
+                  color: Colors.grey[300],
+                  ),
+                label: Text(
+                  'Edit Location',
+                  style: TextStyle(
+                    color: Colors.grey[300],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        color: Colors.white,
+                        letterSpacing: 2.0,
+                      )
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 66,
+                  ),
+                ),
+
+            ],
+            ),
+        ),
+      )
+        ),
+      
+    );
+  }
+}
